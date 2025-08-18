b65d990cc8522b97fae17e63229a13b5384af134
albim72 <marcin.albiniak72@gmail.com>
2025-08-18 14:50:06 +0200

    Update Python_krotki.py

--- DZIEN_1/Python_krotki.py
+++ DZIEN_1/Python_krotki.py
@@ -15,3 +15,37 @@ if miejsce in rezerwacje:
     print(f"miejsce {miejsce} jest zarezerwowane")
 else:
     print(f"miejsce {miejsce} jest dostepne")
+
+#konwersja tuple -> list
+nb = [41,55,66]
+liczby = list(liczby)
+liczby.extend(nb)
+liczby = tuple(liczby)
+print(liczby)
+
+#analiza zmiennej tekstowej
+s = "lajkonik"
+print(s)
+print(s[1])
+print(s[4])
+print(s[1:5])
+print(s[-1])
+print(s[::-1])
+
+print(liczby)
+
+print(liczby[2:9:2])
+print(liczby[-2:-9:-2])
+
+slaj = list(s)
+print(slaj)
+
+slaj.append("i")
+
+print(slaj)
+
+s = str(slaj)
+print(s) #str nie łączy elementów listy - wpływa na kazdy element osobno
+
+sk = "".join(slaj)
+print(sk)
