2a8f77abff344054c7350dc1a6fa3fc8c8970091
albim72 <marcin.albiniak72@gmail.com>
2025-08-19 10:29:21 +0200

    Create funkcje_intro.py

---
+++ DZIEN_2/funkcje_intro.py
@@ -0,0 +1,12 @@
+"""
+opiszemy grupę funkcji
+pokażemy różne aspekty wykorzystanie funkcji w Pythonie
+"""
+
+#prosta funkcja bez argumentów
+def powitanie():
+    print("Witaj w świecie Pythona!")
+
+#przykładowe użuycia funkcji
+if __name__ == '__main__':
+    powitanie()
