fabcbcc7e7a9fd03c3bbefa1b391c0a61cabcc29
albim72 <marcin.albiniak72@gmail.com>
2025-08-19 11:51:30 +0200

    Update funkcyjnie.py

--- DZIEN_2/funkcja_vs_paradygmat/funkcyjnie.py
+++ DZIEN_2/funkcja_vs_paradygmat/funkcyjnie.py
@@ -4,9 +4,10 @@ liczby = list(range(1, 11))
 print(liczby)
 
 wynik = reduce(
-    lambda acc,x: acc + x,
-    map(lambda x: x**2, filter(lambda x: x % 2 == 0, liczby)),
-    0
+    lambda acc,x: acc + x, #sumowanie kolejnch elementów pobranych z funkcji map
+    map(lambda x: x**2, filter(lambda x: x % 2 == 0, liczby)), #funkcja map pobiera elementy z funkcji filter
+    # i podnosi do kwadratu, funkcja filter wybiera tylko liczby parzyste z listy
+    0 # wynik dla listy pustej
 )
 
 print(f"wynik: {wynik}")
