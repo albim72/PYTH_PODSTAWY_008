6758e38d2f6bae01e5cbf16defaec8235ccee5ee
albim72 <marcin.albiniak72@gmail.com>
2025-08-19 11:08:47 +0200

    Update funkcje_intro.py

--- DZIEN_2/funkcje_intro.py
+++ DZIEN_2/funkcje_intro.py
@@ -7,6 +7,34 @@ pokażemy różne aspekty wykorzystanie funkcji w Pythonie
 def powitanie():
     print("Witaj w świecie Pythona!")
 
+#funlcja z jednym argumentem
+def kwadrat(x):
+    return x**2
+#funkcja z dwoma argumentami
+def suma(a,b):
+    return a+b
+
+#funkcja z wartością domyślną
+def powitanie_uzytkownika(imie,jezyk="PL"):
+    if jezyk=="PL":
+        print(f"Witaj, {imie}!")
+    elif jezyk=="EN":
+        print(f"Hello, {imie}!")
+    else:
+        print(f"[{jezyk}] Nieobsługiwany język. Cześc {imie}!")
+
 #przykładowe użuycia funkcji
 if __name__ == '__main__':
+    print("_____ funkcja - powitanie_____")
     powitanie()
+    print("_____ funkcja - kwadrat_____")
+    print(f"kwadrat(7) = {kwadrat(7)}")
+    print("_____ funkcja - suma_____")
+    print(f"suma(2,3) = {suma(2,3)}")
+    print("_____ funkcja - powitanie_uzytkownika_____")
+    print(f"powitanie_uzytkownika('Marek') ->")
+    powitanie_uzytkownika('Marek')
+    print(f"powitanie_uzytkownika('Marek', 'EN') ->")
+    powitanie_uzytkownika('Marek', 'EN')
+    print(f"powitanie_uzytkownika('Marek', 'DE') ->")
+    powitanie_uzytkownika('Marek', 'DE')
