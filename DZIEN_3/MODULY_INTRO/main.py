d0974b08d7668003d31cf66896c2b76089a49000
albim72 <marcin.albiniak72@gmail.com>
2025-08-20 10:01:12 +0200

    Create main.py

---
+++ DZIEN_3/MODULY_INTRO/main.py
@@ -0,0 +1,27 @@
+# import dane
+# import dane as dn
+import math
+
+
+from dane import liczby, leader, MC as m, kolor
+
+# from dane import *
+
+print(liczby)
+print("_"*50)
+print(leader)
+print("_"*50)
+print(f"stała MC = {m}")
+MC = "listopad"
+
+print(f"stała MC = {MC}")
+print(f"stała MC = {m}")
+
+print("_"*50)
+print(f"sin(11) = {math.sin(11)}")
+
+print("_"*50)
+print(f"kolor z danych: {kolor}")
+
+
+
