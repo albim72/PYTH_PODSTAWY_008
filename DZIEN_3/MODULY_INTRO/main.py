e90786c1dabd42fbba29e369a54903dbecb87fcb
albim72 <marcin.albiniak72@gmail.com>
2025-08-20 10:23:39 +0200

    Update main.py

--- DZIEN_3/MODULY_INTRO/main.py
+++ DZIEN_3/MODULY_INTRO/main.py
@@ -1,11 +1,10 @@
 # import dane
 # import dane as dn
+import copy
 import math
-
-
 from dane import liczby, leader, MC as m, kolor
-
 # from dane import *
+from mfunkcje.kwadraty import kwadrat,parzyste,slownik
 
 print(liczby)
 print("_"*50)
@@ -22,6 +21,14 @@ print(f"sin(11) = {math.sin(11)}")
 
 print("_"*50)
 print(f"kolor z danych: {kolor}")
+
+print("_"*50)
+lb = copy.deepcopy(liczby) #kopia głęboka - towrzy nowy obekt typu zadanego - lista z uwzględnieniem zgniezdzeń
+print(f"liczby = {lb}")
+print(lb is liczby)
+print(f"kwadrat(lb) = {kwadrat(lb)}")
+print(f"parzyste(lb) = {parzyste(lb)}")
+print(f"slownik(leader) = {slownik(leader)}")
 
 
 
