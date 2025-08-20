ac6d173801b0770283850bb3075fdc6fe1e4dd01
albim72 <marcin.albiniak72@gmail.com>
2025-08-20 13:35:07 +0200

    Update zwierz.py

--- DZIEN_3/ABSTRAKCJA/zwierz.py
+++ DZIEN_3/ABSTRAKCJA/zwierz.py
@@ -1,17 +1,39 @@
-from abc import ABC, abstractmethod 
+from abc import ABC, abstractmethod
 
 class Zwierze(ABC):
     def __init__(self,name):
         self._name = name
         self.createnew_animal(self._name)
-    
+
     @abstractmethod
     def daj_glos(self):
         pass
-    
+
     @abstractmethod
     def liczba_nog(self):
         return 4
-    
+
     def createnew_animal(self,name):
         return f"utworzono nowy obiekt {name} klasy Zwierze"
+
+
+class Pies(Zwierze):
+    def __init__(self,name):
+        super().__init__(name)
+
+    def daj_glos(self):
+        return "Hau!Hau!"
+
+    def liczba_nog(self):
+        return super().liczba_nog()
+
+
+print("_______ Pies _________")
+p = Pies("Ludvik")
+print(p)
+print(p.daj_glos())
+print(p.liczba_nog())
+
+
+
+
