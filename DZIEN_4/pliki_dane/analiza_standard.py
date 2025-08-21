7b665402b6cf9f9c83c1150b79659261d95c0ccb
albim72 <marcin.albiniak72@gmail.com>
2025-08-21 15:09:35 +0200

    Update analiza_standard.py

--- DZIEN_4/pliki_dane/analiza_standard.py
+++ DZIEN_4/pliki_dane/analiza_standard.py
@@ -19,8 +19,32 @@ def main():
     print("pierwsze 5 filmów")
     for m in movies[:5]:
         print(m["title"], m["year"],m["genre"], m["rating"])
-        
-    
+
+    #filmy po roku 2015
+    after_2015 = [m for m in movies if m["year"] >= 2015]
+    print(f"liczba filmów po roku 2015: {len(after_2015)}")
+
+    #top 10 filmów pod kątem ratingu
+    top10 = sorted(movies, key=lambda m: m["rating"], reverse=True)[:10]
+    print("_____________________________________________________")
+    print("top 10 filmów pod kątem ratingu")
+    for m in top10:
+        print(m["title"], m["year"],m["genre"], m["rating"])
+
+    #filmy z Polski
+    poland = [m for m in movies if m["country"] == "Poland"]
+    print("_____________________________________________________")
+    print(f"filmy z Polski: {len(poland)}")
+
+    #Średnia ocena
+    avg_rating = sum(m["rating"] for m in movies)/len(movies)
+    print("_____________________________________________________")
+    print(f"Średnia ocena: {avg_rating}")
+
+    #film z największym budżetem
+    max_bugdet = max(movies, key=lambda m: m["budget_usd"])
+    print("_____________________________________________________")
+    print(f"film z największym budżetem: {max_bugdet['title'],max_bugdet['budget_usd']}")
 
 if __name__ == '__main__':
     main()
