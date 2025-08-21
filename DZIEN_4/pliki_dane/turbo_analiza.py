cb74b6fb9f0d9c36da7081f52d1f7ef59054afa6
albim72 <marcin.albiniak72@gmail.com>
2025-08-21 16:00:04 +0200

    Update turbo_analiza.py

--- DZIEN_4/pliki_dane/turbo_analiza.py
+++ DZIEN_4/pliki_dane/turbo_analiza.py
@@ -18,8 +18,19 @@ def main():
     print(f"np - średnia ocen: {np.mean(ratings)}")
     print(f"np - mediana ocen: {np.median(ratings)}")
     print(f"np - odchylenie standardowe ocen: {np.std(ratings)}")
+    print("___________________________________________")
+    print("ROI(Return on Investment) = revenue/budget")
+    roi = np.divide(revenues,budgets,out=np.zeros_like(revenues,dtype=float),where=budgets>0)
+    df['ROI'] = roi
+    #top 10 filmów wg ROI
+    top10 = df.sort_values(by='ROI',ascending=False)[:10]
+    print("\n______________ NUMPY ________________")
+    print(top10[['title','year','revenue_usd','budget_usd','ROI']])
 
-    print("ROI")
+    #Grupowanie
+    genre_groups = df.groupby('genre')["rating"].apply(lambda x: np.mean(x.to_numpy()))
+    print("\n______________ średnia ocena wg gatunku ________________")
+    print(genre_groups)
 
 if __name__ == '__main__':
     main()
